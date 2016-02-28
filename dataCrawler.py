bd1c61ee144551adf76c0dd2d373b1ec043fb649
PParaskeva <pnagiotis@yahoo.gr>
2016-02-28 14:34:01 +0000

    update the data crawler

--- dataCrawler.py
+++ dataCrawler.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 forbiden=["<",">",":","/","\\","|","?","*"]
 url="http://www.bbc.co.uk"
 r=requests.get(url+"/news")
-soup=BeautifulSoup(r.content)
+soup=BeautifulSoup(r.content,"lxml")
 temp=soup.find_all("a",{"class":"navigation-wide-list__link"})
 NewsTopics=[]
 NewsTopicsLinks=[]
@@ -16,26 +16,37 @@ temp=[]
 count=0
 for i in NewsTopicsLinks:
     r=requests.get(url+i)
-    soup=BeautifulSoup(r.content)
+    soup=BeautifulSoup(r.content,"lxml")
     temp=soup.find_all("a",{"class":"title-link"})
-    count=count+1
+    
     for j in temp:
         if not("http" in j.get("href") ):
             r=requests.get(url+j.get("href"))
-            soup=BeautifulSoup(r.content)
+            soup=BeautifulSoup(r.content,"lxml")
 
             articleContext=soup.find_all("div",{"class":"story-body"})
+            print("The count is : "+str(count)+" "+"The len of the list is :"+str(len(NewsTopics)))
             n=NewsTopics[count].replace("\n","")
             nn=j.text.replace("\n","")
             for h in forbiden:
                 nn=nn.replace(h," ")
             if not os.path.exists(n):
                 os.makedirs(n)
-            
-            f=open(n+"\\"+nn+".txt","w")
+                
+            #If it is windows
+            #f=open(n+"\\"+nn+".txt","w")
+                
+            #if it is Linux
+                
+            f=open(n+"/"+nn+".txt","w")
 
             for k in articleContext:
-                f.write(k.text)
+                message=k.text.encode("utf-8")
+                f.write(message)
+            f.close()
+            
+    count=count+1
+                
                 
                       
         
