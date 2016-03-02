223550ff5b9d752d79e6c9f3d8a0e2b267137f53
PParaskeva <pnagiotis@yahoo.gr>
2016-03-02 17:26:16 +0000

    I have add the date and time

--- dataCrawler.py
+++ dataCrawler.py
@@ -1,6 +1,9 @@
 import requests
+import urllib
+import urllib2
 import os
 from bs4 import BeautifulSoup
+from time import gmtime, strftime
 #directory="M:\group"
 forbiden=["<",">",":","/","\\","|","?","*"]
 url="http://www.bbc.co.uk"
@@ -25,7 +28,7 @@ for i in NewsTopicsLinks:
             soup=BeautifulSoup(r.content,"lxml")
 
             articleContext=soup.find_all("div",{"class":"story-body"})
-            print("The count is : "+str(count)+" "+"The len of the list is :"+str(len(NewsTopics)))
+            #print("The count is : "+str(count)+" "+"The len of the list is :"+str(len(NewsTopics)))
             n=NewsTopics[count].replace("\n","")
             nn=j.text.replace("\n","")
             for h in forbiden:
@@ -39,12 +42,32 @@ for i in NewsTopicsLinks:
             #if it is Linux
                 
             f=open(n+"/"+nn+".txt","w")
+            paragraphList=[]
 
             for k in articleContext:
-                message=k.text.encode("utf-8")
-                f.write(message)
+                #me=k.text.encode("utf-8")
+                
+                #for q in k.find_all("p"):
+                    #f.write(q)
+                date=""
+                dates=k.find_all("div",{"class":"date date--v2"})
+                if len(dates)==0:
+                    print(strftime("%Y-%d-%m", gmtime()))
+                else:
+                    for q in dates:
+                        print(q['data-datetime'])
+
+                #for date in dates:
+                    #print(date)
+                images =k.find_all("img",{"class":"js-image-replace"})
+                #for image in images:
+                    #urllib.urlretrieve(image['src'], filename=n+"/"+nn+image['alt'])
+                
+            print("----------------------------------------------")
+
             f.close()
-            
+
+
     count=count+1
                 
                 
